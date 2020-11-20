369131bae85b01b6d236ddd22bce2c56cc5178e4
Bob Fornal <bob.fornal@leadingedje.com>
2020-11-19 19:58:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Escape Button Images
+
+The included images can be pulled down and used with the [ESCAPE-BUTTON project](https://github.com/bob-fornal/escape-button).
+
+There should be a `code.js` file in each folder to show the image use and link addresses.
---
+++ google-images-001/code.js
@@ -0,0 +1,11 @@
+
+const images = [
+  { file: 'google-header.webp', location: 'https://www.google.com/search?q=coffee&oq=coffee' },
+  { file: 'coffee-wikipedia.webp', location: 'https://en.wikipedia.org/wiki/Coffee' },
+  { file: 'coffee-nca.webp', location: 'https://www.ncausa.org/About-Coffee/What-is-Coffee' },
+  { file: 'coffee-benefits.webp', location: 'https://www.medicalnewstoday.com/articles/270202' },
+  { file: 'coffee-amazon.webp', location: 'https://www.amazon.com/coffee/s?k=coffee' },
+  { file: 'coffee-peets.webp', location: 'https://www.peets.com/coffee' },
+  { file: 'coffee-stumptown.webp', location: 'https://www.stumptowncoffee.com/' },
+  { file: 'coffee-footer.webp', location: 'https://www.google.com/search?q=coffee&oq=coffee' }
+];
